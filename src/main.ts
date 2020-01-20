056d2288b9d04e0624456370d316cb1d91fdc3f3
anurag4u80 <anurag4u80@gmail.com>
2020-01-20 17:00:28 +0530

    added auth, signup and password hash

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import express from "express";
 import path from "path";
 import { Db } from "mongodb";
 var bodyParser = require('body-parser')
+var passwordHash = require('password-hash');
+
 
 const app = express();
 const port = 8080; // default port to listen
 
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://root:example@localhost:27017/pehchaan";
+var url = "mongodb://root:example@localhost:27017";
+
 async function serverStart() {
     let db = await MongoClient.connect(url,{ useNewUrlParser: true });
+
+    let pehchanDB = db.db("pehchaan")
 
     app.use(bodyParser.json());
     var jwt = require('jsonwebtoken');
@@ -20,8 +25,10 @@ async function serverStart() {
     });
 
 
-    app.post("/login", (req, res) => {
-        if (auth(req.body.username, req.body.password, db)) {
+    app.post("/login",  async (req, res) => {
+        let stat = await auth(req.body.email, req.body.password, pehchanDB);
+        console.log(stat)
+        if (stat) {
             var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
             res.send({ token: token })
         } else {
@@ -30,8 +37,13 @@ async function serverStart() {
     });
 
 
-    app.post("/signup", (req, res) => {
-        res.send({ message: "signup" })
+    app.post("/signup", async (req, res) => {
+        let status = await signUP(req.body.email,req.body.password,pehchanDB)
+
+        if(status)
+            res.send({message:"Signup done"})
+        else
+            res.send({message:"Signup error"})
     });
 
 
@@ -57,21 +69,49 @@ async function serverStart() {
 
 }
 
-function auth(email, password, db) {
+async function auth(email, password, db) {
     //TODO Connect to db and check
-    if (email == "hello") {
+
+    let user = await db.collection("user").findOne({email:email})
+
+    console.log("user:", user)
+    console.log("email:", email)
+    console.log("password:", password)
+
+    if (user) {
+        if (passwordHash.verify(password,user.password)){
+            return true
+        }else{
+            return false
+        }
         return true
     }
     return false
 
 }
 
-function sgnUP(email, password,db) {
+async function signUP(email, password,db) {
     //check if email exists in User
     //if yes return exist error
     //if no create new user
     //OTP if possible
 
+    var hashedPassword = passwordHash.generate(password);
+
+    let user = await db.collection("user").findOne({
+        email:email,
+    })
+
+    if(user) {
+        return false
+    } 
+
+    await db.collection("user").insertOne({
+        email:email,
+        password:hashedPassword
+    })
+
+    return true
 
 }
 
